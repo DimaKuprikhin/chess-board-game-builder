a6f66aaa68d5da6aa349f6e2f98354a96104d331
Dmitry Kuprikhin <dimakuprikhin@gmail.com>
2022-08-20 00:21:57 +0300

    Add logger.

---
+++ logger.py
@@ -0,0 +1,18 @@
+import enum
+import sys
+
+class LogLevel(enum.Enum):
+    INFO = 1
+    WARNING = 2
+    ERROR = 3
+
+def LOG(log_level, message):
+    actual_message = ''
+    if log_level == LogLevel.INFO:
+        actual_message += 'INFO: '
+    elif log_level == LogLevel.WARNING:
+        actual_message += 'WARNING: '
+    elif log_level == LogLevel.ERROR:
+        actual_message += 'ERROR: '
+    actual_message += message
+    print(actual_message, file=sys.stderr)
