8b36fc27d060a31036bd64ff6ff2c08d67af17a4
Dmitry Kuprikhin <dimakuprikhin@gmail.com>
2022-08-21 13:52:59 +0300

    Add Executer class, exception handling in ExecuterManager and code style.

--- executer.py
+++ executer.py
@@ -2,18 +2,23 @@ import script
 import sys
 from logger import LogLevel, LOG
 
-def run():
+
+class Executer:
+  def __init__(self):
+    return
+
+  def run(self):
     LOG(LogLevel.INFO, 'executer has been started')
     while True:
-        command = input()
-        LOG(LogLevel.INFO, 'executer has read input')
-        if command == 'getArray':
-            print(script.getArray(3))
-        elif command == 'getMap':
-            print(script.getMap(4, 'value'))
-        elif command == 'getSquare':
-            print(script.getSquare(7))
-        else:
-            break
-        # There is no need to flush, but we do it.
-        sys.stdout.flush()
+      command = input()
+      LOG(LogLevel.INFO, 'executer has read input')
+      if command == 'getArray':
+        print(script.getArray(3))
+      elif command == 'getMap':
+        print(script.getMap(4, 'value'))
+      elif command == 'getSquare':
+        print(script.getSquare(7))
+      else:
+        break
+      # There is no need to flush, but we do it just in case.
+      sys.stdout.flush()
