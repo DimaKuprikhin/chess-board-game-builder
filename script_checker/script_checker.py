a24abf1e9eb23b6c98233c0206b517fd6a89f94b
Dmitry Kuprikhin <dimakuprikhin@gmail.com>
2022-09-03 12:55:59 +0300

    Add ScriptChecker stub.

---
+++ script_checker/script_checker.py
@@ -0,0 +1,13 @@
+class ScriptChecker:
+  '''
+  Checks scripts for possible malwareness.
+  '''
+  def __init__(self):
+    return
+
+  def check_script(module_name: str) -> bool:
+    '''
+    Returns true if script located in `module_name` is considered safe and
+    contains no malware instructions.
+    '''
+    return True
