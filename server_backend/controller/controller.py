95963cdeacb721b979f549b4621d32078b9e7c45
Dmitry Kuprikhin <dimakuprikhin@gmail.com>
2022-09-10 19:57:57 +0300

    Change Controller to use sqlite db.

--- server_backend/controller/controller.py
+++ server_backend/controller/controller.py
@@ -1,59 +1,36 @@
 import pathlib
-from typing import Any, Tuple
+import sqlite3
 from server_backend.database import games_database
 from server_backend.database import scripts_database
 from server_backend.script_checker import script_checker
+from typing import Any, Tuple
 
 
 class Controller:
-  def __init__(self):
-    self.games_db = None
-    self.script_checker = script_checker.ScriptChecker()
-    self.scripts_db = None
+  def __init__(self, base_module_name: str, scripts_dir: pathlib.PosixPath):
+    self.base_module_name = base_module_name
+    self.scripts_dir = scripts_dir
 
-  def create_game(self, user_id: int, script_id: int,
-                  game_state) -> Tuple[bool, str]:
+  def create_game(
+      self, db: sqlite3.Connection, user_id: int, script_id: int
+  ) -> int:
     '''
-    Creates a game that uses script with the given script id. User id is used
-    as a primary key for games and it means that one user can't create more
-    than one game at the same time. Script id must be a result of previously
-    called `load_script`. On success returns True, otherwise False and failure
-    message.
+    Creates a game that uses script with the given script id. Script id must be
+    a result of previously called `load_script`. Returns game id that should be
+    used for futher requests.
     '''
-    assert self.games_db
-    assert self.scripts_db
-    if not self.scripts_db.contains(script_id):
-      return False, 'Can\'t create a game with incorrect script id'
-    if not self.games_db.add_game(user_id, script_id, game_state):
-      return False, 'The same user can\'t create multiple games at the same time'
-    return True, 'OK'
+    return games_database.add_game(db, user_id, script_id)
 
-  def init_games_database(self):
-    '''
-    Initializes games database. Must be called only once for each Controller.
-    '''
-    assert not self.games_db
-    self.games_db = games_database.GamesDatabase()
-
-  def init_scripts_database(self, data_dir: pathlib.PosixPath):
-    '''
-    Initializes scripts database in the given directory path. Must be called
-    only once for each Controller.
-    '''
-    assert not self.scripts_db
-    self.scripts_db = scripts_database.ScriptsDatabase(
-        data_dir, 'user_scripts'
-    )
-
-  def load_script(self, script: str) -> Tuple[bool, Any]:
+  def load_script(self, db: sqlite3.Connection, user_id: int,
+                  script: str) -> Tuple[bool, Any]:
     '''
     Calls by flask frontend to check user script and save it to database, if it
-    is valid. On success, returns true status and script id, which should be
-    used for futher requests. On failure, returns false status and failure
-    message. `init_scripts_database` must be called before this method.
+    is valid. On success, returns True status and script id, which should be
+    used for futher requests. On failure, returns False status and failure
+    message.
     '''
-    assert self.scripts_db
-    if not self.script_checker.check_script(script):
-      return False, 'Script hasn\'t managed to pass validation check'
-    script_id = self.scripts_db.add_script(script)
-    return True, script_id
+    if not script_checker.check_script(script):
+      return False, 'Script didn\'t manage to pass security check'
+    return True, scripts_database.add_script(
+        db, user_id, script, self.base_module_name, self.scripts_dir
+    )
--- server_backend/controller/controller_test.py
+++ server_backend/controller/controller_test.py
@@ -1,79 +1,26 @@
 from server_backend.controller.controller import *
-import pytest
 
 
 class TestContoller:
-  def test_init_dbs_multiple_times(self, tmp_path: pathlib.PosixPath):
-    controller = Controller()
-    controller.init_games_database()
-    with pytest.raises(AssertionError):
-      controller.init_games_database()
-    with pytest.raises(AssertionError):
-      controller.init_games_database()
-
-    controller.init_scripts_database(tmp_path)
-    with pytest.raises(AssertionError):
-      controller.init_scripts_database(tmp_path)
-    with pytest.raises(AssertionError):
-      controller.init_scripts_database(tmp_path)
+  def _get_db(self) -> sqlite3.Connection:
+    DB_SETUP_SCRIPT_PATH = './flaskr/schema.sql'
+    db = sqlite3.connect(':memory:', detect_types=sqlite3.PARSE_DECLTYPES)
+    db.row_factory = sqlite3.Row
+    with open(DB_SETUP_SCRIPT_PATH, 'r') as f:
+      db.executescript(f.read())
+    return db
 
   def test_load_script(self, tmp_path: pathlib.PosixPath):
-    controller = Controller()
-    controller.init_scripts_database(tmp_path)
-    assert controller.load_script('')[0]
-
-  def test_load_script_without_init_db(self):
-    controller = Controller()
-    with pytest.raises(AssertionError):
-      controller.load_script('')
+    controller = Controller('', tmp_path)
+    db = self._get_db()
+    status, script_id = controller.load_script(db, 1, '')
+    assert status
+    assert isinstance(script_id, int)
 
   def test_create_game(self, tmp_path: pathlib.PosixPath):
-    controller = Controller()
-    controller.init_games_database()
-    controller.init_scripts_database(tmp_path)
-    status, script_id = controller.load_script('')
+    controller = Controller('', tmp_path)
+    db = self._get_db()
+    status, script_id = controller.load_script(db, 1, '')
     assert status
-    controller.create_game(1, script_id, '')
-
-    status, script_id = controller.load_script('')
-    assert status
-    controller.create_game(2, script_id, '')
-
-  def test_create_game_without_init_dbs(self, tmp_path: pathlib.PosixPath):
-    controller = Controller()
-    with pytest.raises(AssertionError):
-      controller.create_game(0, 100, '')
-
-    controller = Controller()
-    controller.init_scripts_database(tmp_path)
-    with pytest.raises(AssertionError):
-      controller.create_game(1, 101, '')
-
-    controller = Controller()
-    controller.init_games_database()
-    with pytest.raises(AssertionError):
-      controller.create_game(2, 102, '')
-
-  def test_create_game_with_invalid_script_id(
-      self, tmp_path: pathlib.PosixPath
-  ):
-    controller = Controller()
-    controller.init_games_database()
-    controller.init_scripts_database(tmp_path)
-    status, message = controller.create_game(1, 101, '')
-    assert not status
-    assert message == 'Can\'t create a game with incorrect script id'
-
-  def test_create_multiple_games_with_same_user_id(
-      self, tmp_path: pathlib.PosixPath
-  ):
-    controller = Controller()
-    controller.init_games_database()
-    controller.init_scripts_database(tmp_path)
-    status, script_id = controller.load_script('')
-    assert status
-    status, message = controller.create_game(1, script_id, '')
-    assert status
-    status, message = controller.create_game(1, script_id, '')
-    assert not status
-    assert message == 'The same user can\'t create multiple games at the same time'
+    assert isinstance(script_id, int)
+    controller.create_game(db, 1, script_id)
