f206fe6b055ab56a4429343641e84c1f6263f6fc
Dmitry Kuprikhin <dimakuprikhin@gmail.com>
2022-09-10 22:15:47 +0300

    Change [de]serialization of request and response.

--- flask_tests/load_script_test.py
+++ flask_tests/load_script_test.py
@@ -5,11 +5,15 @@ from flask import Flask
 
 
 def test_register(client: Client, app: Flask):
-    response = client.post('/load_script/', json={'script': 'print(2)', 'user_id': 1})
-    assert response.status_code == 200
-    script_id = json.loads(response.get_data(as_text=True))['script_id']
-    assert isinstance(script_id, int)
-    with app.app_context():
-        assert get_db().execute(
-            "SELECT id FROM scripts WHERE user_id == ?;", [1]
-        ).fetchone()['id'] == script_id
+  response = client.post(
+      '/load_script/', json={
+          'script': 'print(2)',
+          'user_id': 1
+      }
+  )
+  assert response.status_code == 200
+  script_id = json.loads(response.get_data(as_text=True))['script_id']
+  assert isinstance(script_id, int)
+  with app.app_context():
+    assert get_db().execute("SELECT id FROM scripts WHERE user_id == ?;",
+                            [1]).fetchone()['id'] == script_id
--- flaskr/load_script.py
+++ flaskr/load_script.py
@@ -11,21 +11,13 @@ load_script_bp = Blueprint('load_script', __name__, url_prefix='/load_script')
 def load_script():
   if request.content_length >= 1024 * 1024:
     return (400, 'Data is too big')
-  user_id, script = deserialize_load_script_request(
+  request_json = json.loads(
       request.get_data(cache=False, as_text=True, parse_form_data=False)
   )
   controller = Controller('scripts', pathlib.PosixPath('.', 'scripts'))
-  status, result = controller.load_script(db.get_db(), user_id, script)
+  status, result = controller.load_script(
+      db.get_db(), request_json['user_id'], request_json['script']
+  )
   if status:
-    return serialize_load_script_response(result)
+    return json.dumps({ 'script_id': result })
   return (400, result)
-
-
-def deserialize_load_script_request(request):
-  print(request)
-  request = json.loads(request)
-  return request['user_id'], request['script']
-
-
-def serialize_load_script_response(script_id):
-  return json.dumps({ 'script_id': script_id })
