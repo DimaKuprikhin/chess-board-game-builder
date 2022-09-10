227a2a1d670d8ee502a5df6758cb7bcb3b835488
Dmitry Kuprikhin <dimakuprikhin@gmail.com>
2022-09-10 19:24:43 +0300

    Change scripts database to use sqlite db.

--- server_backend/database/scripts_database.py
+++ server_backend/database/scripts_database.py
@@ -1,77 +1,73 @@
 import os
 import pathlib
+import random
+import string
+import sqlite3
 
 
-class ScriptsDatabase:
-  class Entry:
-    def __init__(self, module_name: str, script_path: str):
-      self.module_name = module_name
-      self.script_path = script_path
+def _generate_unique_string(n: int) -> str:
+  return ''.join(
+      random.choice(string.ascii_letters + string.digits) for _ in range(n)
+  )
 
-    def get_module_name(self) -> str:
-      return self.module_name
 
-    def get_script_path(self) -> str:
-      return self.script_path
+def add_script(
+    db: sqlite3.Connection, user_id: int, script: str, base_module_name: str,
+    scripts_dir: pathlib.PosixPath
+) -> int:
+  '''
+  Adds user script to scripts table, creates a unique module name and
+  a file in `scripts_dir`. On success, returns script id of the added script.
+  '''
+  script_name = _generate_unique_string(3)
+  module_name = base_module_name + '.' + script_name
+  script_path = scripts_dir.joinpath(script_name + '.py').as_posix()
+  db.execute(
+      'INSERT INTO scripts (user_id, module_name, script_path) VALUES (?, ?, ?);',
+      [user_id, module_name, script_path]
+  )
+  db.commit()
+  # TODO: is there some better way to do it?
+  script_id = db.execute(
+      'SELECT id FROM scripts ORDER BY id DESC LIMIT 1;', []
+  ).fetchone()['id']
+  with open(script_path, 'w') as f:
+    f.write(script)
+  return script_id
 
-  def __init__(
-      self, db_dir_path: pathlib.PosixPath, base_module_name: str = ''
-  ):
-    '''
-    Creates a directory with path `db_dir_path` which will contain file for all
-    the stored scripts. These scripts can be imported by other modules by
-    module names got from `get_module_name` method. `base_module_name` will be
-    a prefix for module names of stored scripts and must correspond
-    `db_dir_path`.
-    '''
-    assert isinstance(db_dir_path, pathlib.PosixPath)
-    assert isinstance(base_module_name, str)
-    self.db_dir_path = db_dir_path
-    if not os.path.exists(db_dir_path):
-      os.mkdir(db_dir_path)
-    self.base_module_name = base_module_name
-    self.next_free_id = 1
-    self.scripts = {}
 
-  def add_script(self, script: str) -> int:
-    '''
-    Adds user script to scripts database, creates a unique module name and
-    a file in `db_dir_path`. On success, returns script id, which can be used
-    to access this database data.
-    '''
-    script_id = self.next_free_id
-    self.next_free_id += 1
-    module_name = self.base_module_name + '.' + str(script_id)
-    script_path = self.db_dir_path.joinpath(str(script_id) + '.py')
-    self.scripts[script_id] = ScriptsDatabase.Entry(module_name, script_path)
-    with open(script_path, 'w') as f:
-      f.write(script)
-    return script_id
+def contains(db: sqlite3.Connection, script_id: int) -> bool:
+  '''
+  Returns True, if the database contains an entry with the given script id.
+  '''
+  return db.execute('SELECT id FROM scripts WHERE id == ?;',
+                    [script_id]).fetchone() is not None
 
-  def contains(self, script_id: int) -> bool:
-    '''
-    Returns True, if the database contains an entry with the given script id.
-    '''
-    return script_id in self.scripts
 
-  def get_module_name(self, script_id: int) -> str:
-    '''
-    Returns a unique scripts module name, which can be imported in other
-    modules. Returns None, if there is no script with the given script_id in
-    database.
-    '''
-    if script_id not in self.scripts:
-      return None
-    return self.scripts[script_id].get_module_name()
+def get_module_name(db: sqlite3.Connection, script_id: int) -> str:
+  '''
+  Returns a unique scripts module name, which can be imported in other
+  modules. Returns None, if there is no script with the given script id in
+  database.
+  '''
+  result = db.execute(
+      'SELECT module_name FROM scripts WHERE id == ?;', [script_id]
+  ).fetchone()
+  return None if result is None else result['module_name']
 
-  def remove_script(self, script_id: int) -> bool:
-    '''
-    Removes a script entry from the database and script file from `db_dir_path`
-    directory. On success, returns True, otherwise False. May throw exceptions.
-    '''
-    if script_id not in self.scripts:
-      return False
-    path = self.scripts[script_id].get_script_path()
-    self.scripts.pop(script_id)
-    os.remove(path)
-    return True
+
+def remove_script(db: sqlite3.Connection, script_id: int) -> bool:
+  '''
+  Removes a script entry from the database and corresponding script file.
+  On success, returns True. If database didn't contain this script id, returns
+  False.
+  '''
+  script_path = db.execute(
+      'SELECT script_path FROM scripts WHERE id == ?;', [script_id]
+  ).fetchone()
+  if script_path is None:
+    return False
+  db.execute('DELETE FROM scripts WHERE id == ?;', [script_id])
+  db.commit()
+  os.remove(script_path['script_path'])
+  return True
--- server_backend/database/scripts_database_test.py
+++ server_backend/database/scripts_database_test.py
@@ -1,45 +1,55 @@
 import importlib
+from typing import Tuple
 from server_backend.database.scripts_database import *
 
 
 class TestScriptsDatabase:
-  def _get_db(self, tmp_path: pathlib.PosixPath):
-    db = ScriptsDatabase(tmp_path, 'base')
-    assert not db.contains(1)
-    assert not db.remove_script(2)
-    assert not db.get_module_name(3)
+  def _get_db(self) -> sqlite3.Connection:
+    DB_SETUP_SCRIPT_PATH = './flaskr/schema.sql'
+    db = sqlite3.connect(':memory:', detect_types=sqlite3.PARSE_DECLTYPES)
+    db.row_factory = sqlite3.Row
+    with open(DB_SETUP_SCRIPT_PATH, 'r') as f:
+      db.executescript(f.read())
     return db
 
-  def _add_script(self, db: ScriptsDatabase):
-    script_id = db.add_script('')
-    assert db.contains(script_id)
+  def _add_script(
+      self, db: sqlite3.Connection, user_id: int, base_module_name: str,
+      scripts_dir: pathlib.PosixPath
+  ) -> int:
+    script_id = add_script(db, user_id, '', base_module_name, scripts_dir)
+    assert contains(db, script_id)
     assert isinstance(script_id, int)
-    assert db.get_module_name(script_id)
+    assert get_module_name(db, script_id)
     return script_id
 
-  def _remove_script(self, db: ScriptsDatabase, script_id: int):
-    assert db.remove_script(script_id)
-    assert db.get_module_name(script_id) is None
-    assert not db.contains(script_id)
+  def _remove_script(self, db: sqlite3.Connection, script_id: int):
+    assert remove_script(db, script_id)
+    assert get_module_name(db, script_id) is None
+    assert not contains(db, script_id)
 
-  def test_create_db(self, tmp_path: pathlib.PosixPath):
-    db = self._get_db(tmp_path)
+  def test_create_db(self):
+    db = self._get_db()
+    db.execute('SELECT 1;')
 
   def test_add_remove_scripts(self, tmp_path: pathlib.PosixPath):
-    db = self._get_db(tmp_path)
-    first_id = self._add_script(db)
-    second_id = self._add_script(db)
+    db = self._get_db()
+    base_module_name = 'scripts'
+    first_id = self._add_script(db, 1, base_module_name, tmp_path)
+    second_id = self._add_script(db, 1, base_module_name, tmp_path)
     assert first_id != second_id
     self._remove_script(db, second_id)
-    assert db.contains(first_id)
+    assert contains(db, first_id)
     self._remove_script(db, first_id)
 
   def test_import_by_module_name(self):
-    db = ScriptsDatabase(
-        pathlib.PosixPath('server_backend', 'test_data', 'scripts_db_data'),
-        'server_backend.test_data.scripts_db_data'
+    base_module_name = 'server_backend.test_data.scripts_dir'
+    scripts_dir = pathlib.PosixPath(
+        '.', 'server_backend', 'test_data', 'scripts_dir'
     )
-    script_id = db.add_script('')
-    module_name = db.get_module_name(script_id)
+    if not os.path.isdir(scripts_dir):
+      os.mkdir(scripts_dir)
+    db = self._get_db()
+    script_id = add_script(db, 1, 'print(1)', base_module_name, scripts_dir)
+    module_name = get_module_name(db, script_id)
     importlib.import_module(module_name)
-    db.remove_script(script_id)
+    self._remove_script(db, script_id)
