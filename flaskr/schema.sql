a3cc0b716fe77dc01a8bf7def06e2867d34f6884
Dmitry Kuprikhin <dimakuprikhin@gmail.com>
2022-09-25 00:06:06 +0300

    Change database scheme.

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -11,11 +11,15 @@ CREATE TABLE games (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     first_player_id INTEGER NOT NULL,
     second_player_id INTEGER,
-    first_player_plays_as TEXT NOT NULL CHECK (first_player_plays_as == 'white' OR first_player_plays_as == 'black'),
+    first_player_plays_as TEXT NOT NULL CHECK
+        (first_player_plays_as == 'white'
+        OR first_player_plays_as == 'black'),
     move_number INTEGER NOT NULL,
-    turn TEXT NOT NULL CHECK (turn == 'white' OR turn == 'black'),
+    turn TEXT NOT NULL CHECK
+        (turn == 'white' OR turn == 'black'),
     script_id INTEGER NOT NULL,
     link TEXT NOT NULL UNIQUE,
     game_state TEXT,
-    additional_data TEXT
+    additional_data TEXT,
+    FOREIGN KEY (script_id) REFERENCES scripts(id)
 );
